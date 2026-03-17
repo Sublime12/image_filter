25e3f7a54a508bb6054e0ac8f46fae89493fbce1
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-03-17 19:51:17 -0400

    add image crate for processing images and create a simple program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use image::ImageBuffer;
+
 fn main() {
-    println!("Hello, world!");
+
+    let width = 800;
+    let height = 800;
+    let mut imgbuf = ImageBuffer::new(width, height);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r =( 0.3 * x as f32) as u8;
+        let g = 0;
+        let b =( 0.3 * y as f32) as u8;
+
+        *pixel = image::Rgb([r, g, b]);
+    }
+
+    imgbuf.save("image.png").unwrap();
+    println!("Image has been saved");
 }
