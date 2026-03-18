3fe3ad31aae82d662b5a524d5fa55e73c9cca479
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-03-17 21:25:43 -0400

    add example showing how to darken an image

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
-use image::ImageBuffer;
+#![allow(dead_code)]
+mod conv;
+
+
+use image::{ImageBuffer, ImageReader};
 
 fn main() {
+    //generate_gradient_image();
+    load_images();
+}
 
+fn load_images() {
+    let img = ImageReader::open("zebra.jpg").unwrap().decode().unwrap();
+    let mut img = img.to_rgb8();
+
+    for (_, _, pixel) in img.enumerate_pixels_mut() {
+        let r = pixel[0].saturating_sub(50);
+        let g = pixel[1].saturating_sub(50);
+        let b = pixel[2].saturating_sub(50);
+        *pixel = image::Rgb([r, g, b]);
+    }
+
+    img.save("zebra_darker.jpg").unwrap();
+}
+
+fn generate_gradient_image() {
     let width = 800;
     let height = 800;
     let mut imgbuf = ImageBuffer::new(width, height);
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r =( 0.3 * x as f32) as u8;
+        let r = (0.3 * x as f32) as u8;
         let g = 0;
-        let b =( 0.3 * y as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
 
         *pixel = image::Rgb([r, g, b]);
     }
